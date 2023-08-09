121939ba36fa54b4487e8c6023ef378b816c5928
mkaddani linux <ledevloppeurmohammedkaddani@gmail.com>
2023-08-09 15:28:03 +0100

    adding a comment

--- pyccel/codegen/compiling/compilers.py
+++ pyccel/codegen/compiling/compilers.py
@@ -30,7 +30,7 @@ if platform.system() == 'Darwin':
 
 def get_condaless_search_path(conda_warnings = 'basic'):
     """_summary_
-        Lorem Ipsum Dollar
+        Lorem Ipsum Dollar --- test
     """
     path_sep = ';' if platform.system() == 'Windows' else ':'
     current_path = os.environ['PATH']
