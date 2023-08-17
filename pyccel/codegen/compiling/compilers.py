97e8a2c6038fb21c05420a9fff7e7ab4fcebefb0
Mohammed Kaddani <ledevloppeurmohammedkaddani@gmail.com>
2023-08-17 14:53:13 +0100

    Update compilers.py

--- pyccel/codegen/compiling/compilers.py
+++ pyccel/codegen/compiling/compilers.py
@@ -30,7 +30,7 @@ if platform.system() == 'Darwin':
 
 def get_condaless_search_path(conda_warnings = 'basic'):
     """_summary_
-        Lorem Ipsum Dollar --- test @@@@2
+        Lorem Ipsum Dollar --- test @@@@2d
     """
     path_sep = ';' if platform.system() == 'Windows' else ':'
     current_path = os.environ['PATH']
